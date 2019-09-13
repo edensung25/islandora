b7bc79a9529863ba2ca9142a0a52d68a6800c3da
Cai-Cian <cas386@pitt.edu>
2019-09-13 14:19:35 -0400

    add a new form for video streaming options

---
+++ theme/islandora-kaltura.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+echo "123";
+?>
+
+<script src="//cdnapisec.kaltura.com/p/2592481/sp/259248100/embedIframeJs/uiconf_id/44456041/partner_id/2592481"></script>
+
+<div id="kaltura_player" style="width: 560px; height: 395px;"></div>
+  <script>
+    kWidget.embed({
+          "targetId": "kaltura_player",
+          "wid": "_2592481",
+          "uiconf_id": 44456041,
+          "flashvars": {},
+          "entry_id": "1_at3nsjdt"
+        });
+  </script>
